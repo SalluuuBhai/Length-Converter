99553728ade7f6e29c4e97fac8d4e6842015686b
SalluuuBhai <md.salmanfaris03@gmail.com>
2023-01-30 18:21:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function convert(){
+    const cmValue = Number(document.getElementById("input").value)
+    const inchValue = cmValue/2.54
+    const result = document.getElementById("result")
+    result.innerHTML = inchValue.toFixed(3) + " inches"
+}
